fixed rotation matrices and finished p2p calibration. (Note: so3 convention is different that the one I am using)

scripts/camera_casadi_expr.py
@@ -1,6 +1,20 @@
 import numpy as np
 import casadi as ca
-import cv2
+import so3
+
+rot_model2cam = ca.SX(np.array([
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 0],
+    ]))
+
+SE2_pix2image = ca.SX([
+    [-1, 0,  1600],
+    [0, -1, 1200],
+    [0, 0,  1],
+])
+
+inv_SE2 = SE2_pix2image
 
 def euler2dcm_ca(euler):
     #euler 321
@@ -26,7 +40,7 @@ def euler2dcm_ca(euler):
 #     ])
     return dcm
 
-def get_cam_mat_ca(cam_param, cam_pos, cam_att):
+def get_cam_in_ca(cam_param):
     if type(cam_param)==type([]):
         cam_param = np.array(cam_param)
     
@@ -39,11 +53,7 @@ def get_cam_mat_ca(cam_param, cam_pos, cam_att):
     cx = cam_param[2]
     cy = cam_param[5]
     s = cam_param[1]
-    
-    pos = cam_pos
-    euler = cam_att
-    
-    # intrinsic matrix
+
     cam_in = ca.SX(3,3)
     cam_in[0,0] = fx
     cam_in[0,1] = s
@@ -51,20 +61,23 @@ def get_cam_mat_ca(cam_param, cam_pos, cam_att):
     cam_in[1,1] = fy
     cam_in[1,2] = cy
     cam_in[2,2] = 1
+    return cam_in
+
+def get_cam_mat_ca(cam_param, cam_pos, cam_att):    
+    pos = cam_pos
+    euler = cam_att
+    # intrinsic matrix
+    cam_in = get_cam_in_ca(cam_param)
+
     # extrinsic matrix
     rot_world2model = euler2dcm_ca(euler)
 #     rot_world2model = so3.Dcm.from_euler(euler)
-    rot_model2cam = ca.SX(np.array([
-        [0, 1, 0],
-        [0, 0, 1],
-        [-1, 0, 0],
-    ]))
     
     cam_ex = rot_model2cam @ rot_world2model
     block = ca.SX(3,4)
     block[:,0:3] = cam_ex
     block[:,3] = -(cam_ex @ pos)
-    cam_mat = cam_in @ block
+    cam_mat = inv_SE2 @ cam_in @ block
     return cam_mat
 
 #%%
@@ -98,68 +111,112 @@ def so3_exp(v):
     X = so3_wedge(v)
     R = ca.SX.eye(3) + C1(theta)*X + C2(theta)*ca.mtimes(X, X)
     # NOTE: WHY DOES R need a negative sign to match euler321 to DCM ????
-    # return R
-    return -R
+    return R
+    # return -R
 
 def so3_log(R):
-    R = -R
+    # R = -R
     theta = ca.arccos((ca.trace(R) - 1) / 2)
     return vee(C3(theta) * (R - R.T))
 
-def get_cam_mat_lie_ca(cam_param, cam_pos, cam_lie):
-    if type(cam_param)==type([]):
-        cam_param = np.array(cam_param)
-
-    if not cam_param.shape[0] == 9:
-        print("invalid camera parameters please check again!!!!")
-        return None
-
-    fx = cam_param[0]
-    fy = cam_param[4]
-    cx = cam_param[2]
-    cy = cam_param[5]
-    s = cam_param[1]
-    
+def get_cam_mat_lie_ca(cam_param, cam_pos, cam_lie):  
     pos = cam_pos
     w = cam_lie # so3
     
     # intrinsic matrix
-    cam_in = ca.SX(3,3)
-    cam_in[0,0] = fx
-    cam_in[0,1] = s
-    cam_in[0,2] = cx
-    cam_in[1,1] = fy
-    cam_in[1,2] = cy
-    cam_in[2,2] = 1
+    cam_in = get_cam_in_ca(cam_param)
     # extrinsic matrix
-#     rot_world2model = euler2dcm_ca(euler)
-    rot_world2model = so3_exp(w)
-    rot_model2cam = ca.SX(np.array([
-        [0, 1, 0],
-        [0, 0, 1],
-        [-1, 0, 0],
-    ]))
+    rot_world2model = so3_exp(-w)
     
     cam_ex = rot_model2cam @ rot_world2model
     block = ca.SX(3,4)
     block[:,0:3] = cam_ex
     block[:,3] = -(cam_ex @ pos)
-    cam_mat = cam_in @ block
+    cam_mat = inv_SE2 @ cam_in @ block
     return cam_mat
+
+def get_cam_mat_mrp_ca(cam_param, cam_pos, cam_mrp):   
+    pos = cam_pos
+
+    # intrinsic matrix
+    cam_in = get_cam_in_ca(cam_param)
+
+    # extrinsic matrix
+    rot_world2model = so3.Dcm.from_mrp(cam_mrp)
+    
+    cam_ex = rot_model2cam @ rot_world2model
+    block = ca.SX(3,4)
+    block[:,0:3] = cam_ex
+    block[:,3] = -(cam_ex @ pos)
+    cam_mat = inv_SE2 @ cam_in @ block
+    return cam_mat
+
+def get_cam_mat_quat_ca(cam_param, cam_pos, cam_quat):
+    pos = cam_pos
+
+    # intrinsic matrix
+    cam_in = get_cam_in_ca(cam_param)
+
+    # extrinsic matrix
+    rot_world2model = so3.Dcm.from_quat(cam_quat)
+    
+    cam_ex = rot_model2cam @ rot_world2model
+    block = ca.SX(3,4)
+    block[:,0:3] = cam_ex
+    block[:,3] = -(cam_ex @ pos)
+    cam_mat = inv_SE2 @ cam_in @ block
+    return cam_mat
+
+def get_cam_mat_R_ca(cam_param, cam_pos, cam_R):
+    pos = cam_pos
+
+    # intrinsic matrix
+    cam_in = get_cam_in_ca(cam_param)
+
+    # extrinsic matrix
+    rot_world2model = cam_R
+    
+    cam_ex = rot_model2cam @ rot_world2model
+    block = ca.SX(3,4)
+    block[:,0:3] = cam_ex
+    block[:,3] = -(cam_ex @ pos)
+    cam_mat = inv_SE2 @ cam_in @ block
+    return cam_mat
+
+def get_ray_ca(cam_param, cam_lie, pix_hom):
+    K = get_cam_in_ca(cam_param)
+    R = rot_model2cam @ so3_exp(cam_lie) 
+    Bp = ca.inv(K @ R)
+
+    vec = Bp @ pix_hom
+    vec = vec / ca.norm_2(vec)
+    return vec
 
 cam_param_ca = ca.SX.sym('param', 9)
 
 cam_pos_ca = ca.SX.sym('pos', 3)
 cam_att_ca = ca.SX.sym('att', 3)
 cam_lie_ca = ca.SX.sym('lie', 3)
+cam_mrp_ca = ca.SX.sym("mrp", 4)
+cam_quat_ca = ca.SX.sym("quat", 4)
+cam_dcm_ca = ca.SX.sym("dcm", (3,3))
+
+rho_ca = ca.SX.sym("rho", 2) # scaling of image coordinate after projection
 
 cam_mat_ca = get_cam_mat_ca(cam_param_ca, cam_pos_ca, cam_att_ca)
 
 cam_mat_lie_ca = get_cam_mat_lie_ca(cam_param_ca, cam_pos_ca, cam_lie_ca)
+cam_mat_quat_ca = get_cam_mat_quat_ca(cam_param_ca, cam_pos_ca, cam_quat_ca)
 
-f_cam_mat = ca.Function('f_cam_mat',[cam_param_ca, cam_pos_ca, cam_att_ca], [cam_mat_ca])
+f_cam_mat_euler = ca.Function('f_cam_mat',[cam_param_ca, cam_pos_ca, cam_att_ca], [cam_mat_ca])
 f_cam_mat_lie = ca.Function('f_cam_mat_lie',[cam_param_ca, cam_pos_ca, cam_lie_ca], [cam_mat_lie_ca])
-
+f_cam_mat_quat = ca.Function('f_cam_mat_quat', [cam_param_ca, cam_pos_ca, cam_quat_ca], [cam_mat_quat_ca])
+f_lie2euler = ca.Function('f_lie2euler', [cam_lie_ca], [so3.Euler.from_dcm(so3.Dcm.exp(cam_lie_ca))])
+f_lie2dcm = ca.Function('f_lie2dcm', [cam_lie_ca], [so3.Dcm.exp(cam_lie_ca)])
+f_q2dcm = ca.Function('f_q2dcm', [cam_quat_ca], [so3.Dcm.from_quat(cam_quat_ca)])
+f_euler2dcm = ca.Function('f_euler2dcm', [cam_att_ca], [so3.Dcm.from_euler(cam_att_ca)])
+f_dcm2euler = ca.Function('f_dcm2euler', [cam_dcm_ca], [so3.Euler.from_dcm(cam_dcm_ca)])
+f_dcm2lie = ca.Function('dcm2lie', [cam_dcm_ca], [so3.Dcm.log(cam_dcm_ca)])
 # opencv getOptimalNewCamera Matrix 
 # https://github.com/opencv/opencv/blob/8b4fa2605e1155bbef0d906bb1f272ec06d7796e/modules/calib3d/src/calibration.cpp#L2714
 #
@@ -211,3 +268,4 @@ def undistortPoints(points, cameraMatrix, k, P):
         undistort_points[i,1] = y
 
     return undistort_points
+# %%

scripts/ex_calibrate_manual.py
@@ -10,7 +10,7 @@ import cv2
 import util
 
 calib_data = None
-with open(f"{data_dir}/calibration/calib_data.json", 'r') as fs:
+with open(f"{data_dir}/calibration_circle/calib_data.json", 'r') as fs:
     calib_data = json.load(fs)
 cam_poses = calib_data["pose"]
 cam_rois = calib_data["roi"]
@@ -61,13 +61,13 @@ if __name__=="__main__":
     obj_pos_1 = np.array(cam_poses[other_cam_names[0]][0:3])
     obj_pos_2 = np.array(cam_poses[other_cam_names[1]][0:3])
 
-    orig_img = cv2.imread(f"{data_dir}/calibration/{cam_name}_view.jpg")
+    orig_img = cv2.imread(f"{data_dir}/calibration_circle/{cam_name}_view.jpg")
     orig_img = cv2.rectangle(orig_img, rois[0,0:2], (rois[0,0:2] + rois[0,2:4]), (255,0,0), 2)
     orig_img = cv2.rectangle(orig_img, rois[1,0:2], (rois[1,0:2] + rois[1,2:4]), (0,255,0), 2)
 
     cam_att = np.zeros(3)
     cam_pos = np.array(cam_poses[cam_name][0:3])
-    cam_P, dist_valid_roi = cv2.getOptimalNewCameraMatrix(cam_pinhole_K.reshape(3,3), cam_dist, (1600,1200), 0.5, (1600, 1200))
+    cam_P, dist_valid_roi = cv2.getOptimalNewCameraMatrix(cam_pinhole_K.reshape(3,3), cam_dist, (1600,1200), 1, (1600, 1200))
 
     slider_scale = 10000
     undistort_img = cv2.undistort(orig_img, cam_pinhole_K.reshape(3,3), cam_dist, None, newCameraMatrix=cam_P)

scripts/get_calibration_data.py
@@ -23,17 +23,35 @@ cam_names = [
     "camera_3",
     ]
 
+def get_opt_quat(Q):
+    # Q: 4xM
+    print(Q.shape)
+    A = np.zeros((4,4))
+    M = Q.shape[0]
+    for i in range(M):
+        q = Q[i,:].reshape(4,1)
+        if q[0] < 0:
+            q = -q
+        A = q @ q.T + A
+    A = A / M
+    e, v = np.linalg.eig(A)
+    q_avg = v[:,np.argmax(e)]
+    return q_avg.ravel()
+
 def get_cam_world_pos():
     cam_poses = {}
     # get the first 10 pose msgs from qualisys and average them to store as camera's position
     for cam in cam_names:
         pos_list = []
+        quat_list = [] # w x y z
         print(f"getting position of {cam}...")
         for i in range(10):
             msg_: PoseStamped = rospy.wait_for_message(f"/qualisys/{cam}/pose", PoseStamped)
             pos_list.append([msg_.pose.position.x, msg_.pose.position.y, msg_.pose.position.z])
-        
-        cam_poses[cam] = np.hstack([np.mean(pos_list, axis=0), np.zeros(3)]).tolist()
+            quat_list.append([msg_.pose.orientation.w, msg_.pose.orientation.x, msg_.pose.orientation.y, msg_.pose.orientation.z,])
+        q_avg = get_opt_quat(np.array(quat_list))
+        cam_poses[cam] = np.hstack([np.mean(pos_list, axis=0), q_avg]).tolist()
+
     print(cam_poses)
 
     return cam_poses
@@ -69,7 +87,8 @@ def main(args):
     from datetime import date, datetime
     import os, errno
     cam_poses = get_cam_world_pos()
-    cam_rois = get_cam_pixel_pos()
+    if not args.pose_only:
+        cam_rois = get_cam_pixel_pos()
     
     try:
         date_str = f"{date.today().strftime('%Y-%m-%d')}-{datetime.now().time().strftime('%I%M%S')}"
@@ -78,8 +97,11 @@ def main(args):
         fcalib_data = f"{data_dir}/calibration-{date_str}/calib_data.json"
     
         with open(fcalib_data, "w") as fs:
-            data = {"pose": cam_poses, "roi": cam_rois}
-            json.dump(data, fs)
+            if args.pose_only:
+                json.dump(cam_poses, fs)
+            else:
+                data = {"pose": cam_poses, "roi": cam_rois}
+                json.dump(data, fs)
         
         if args.save_img:
             save_cam_view(calib_dir)
@@ -93,6 +115,7 @@ if __name__=="__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("save_img", type=bool)
+    parser.add_argument("--pose_only", type=bool)
     args = parser.parse_args()
     rospy.init_node("extrinsic_calibration")
 

scripts/util.py
@@ -2,10 +2,19 @@ import numpy as np
 
 # rotation correction between gazebo cam coordinates and literature convention
 R_model2cam = np.array([
-        [0, 1, 0],
-        [0, 0, 1],
-        [-1, 0, 0],        
-    ])
+    [0, 1, 0],
+    [0, 0, 1],
+    [1, 0, 0],        
+])
+
+# SE2, transform image coordiantes to match the book
+SE2_pix2image = np.array([
+    [-1, 0,  1600],
+    [0, -1, 1200],
+    [0, 0,  1],
+])
+
+inv_SE2 = SE2_pix2image
 
 # 321 Euler sequence
 def euler2dcm(euler):
@@ -32,6 +41,16 @@ def euler2dcm(euler):
     dcm = R1 @ R2 @ R3
     return dcm
 
+def mrp2dcm(r):
+        assert r.shape == (4, 1) or r.shape == (4,)
+        a = r[:3]
+        X = so3_wedge(a)
+        n_sq = np.dot(a, a)
+        X_sq = X @ X
+        R = np.eye(3) + (8 * X_sq - 4 * (1 - n_sq) * X) / (1 + n_sq) ** 2
+        # return transpose, due to convention difference in book
+        return R.T
+
 def cart2hom(points):
     """
     points shape should be (3, n)
@@ -49,7 +68,6 @@ def so3_wedge(w):
         [w[2], 0, -w[0]],
         [-w[1], w[0], 0]
     ])
-    # NOTE: NOT standard convention but have to add "-" to match euler 321 sequence???
     return wx
 
 def so3_vee(wx):
@@ -69,25 +87,18 @@ def so3_exp(w):
         C2 = 1/2- theta**2/24 + theta**4/720 - theta**5/40320
     wx = so3_wedge(w)
     R = np.eye(3) + C1 * wx + C2 * wx @ wx
-    # NOTE: WHY DOES R need a negative sign to match DCM converted from Euler 321????
-    # return R
-    return -R
+    return R
 
 def so3_log(R):
-    theta = np.arccos((np.linalg.trace(R) - 1) / 2)
+    theta = np.arccos((np.trace(R) - 1) / 2)
     C3 = 0
     if np.abs(theta) > eps:
         C3 = theta/(2*np.sin(theta))
     else:
         C3 = 0.5 + theta**2/12 + 7*theta**4/720
-    return so3_vee(C3(theta) * (R - R.T))
+    return so3_vee(C3 * (R - R.T))
 
 def get_cam_in(cam_param):
-    # fx = cam_param[0]
-    # fy = cam_param[1]
-    # cx = cam_param[2]
-    # cy = cam_param[3]
-    # s = cam_param[4]
     fx = cam_param[0]
     fy = cam_param[4]
     cx = cam_param[2]
@@ -110,19 +121,47 @@ def get_cam_mat_euler(cam_param, cam_pos, cam_euler):
     cam_in = get_cam_in(cam_param)    
     cam_ex = get_cam_ex_euler(cam_pos, cam_euler)
 
-    cam_mat = cam_in @ cam_ex
+    cam_mat = inv_SE2 @ cam_in @ cam_ex
     return cam_mat
 
-def get_cam_ex_lie(cam_pos, cam_att):
-    R_world2model = so3_exp(cam_att)
+def get_cam_ex_lie(cam_pos, cam_lie):
+    R_world2model = so3_exp(-cam_lie)
     cam_ex = R_model2cam @ R_world2model @ np.block([np.eye(3), -cam_pos.reshape(-1,1)])
     return cam_ex
 
-def get_cam_mat_lie(cam_param, cam_pos, cam_att):
+def get_cam_mat_lie(cam_param, cam_pos, cam_lie):
     cam_in = get_cam_in(cam_param)
-    cam_ex = get_cam_ex_lie(cam_pos, cam_att)
-    return cam_in @ cam_ex
+    cam_ex = get_cam_ex_lie(cam_pos, cam_lie)
+    return inv_SE2 @ cam_in @ cam_ex
 
+def get_cam_ex_mrp(cam_pos, cam_mrp):
+    R_world2model = mrp2dcm(cam_mrp)
+    cam_ex = R_model2cam @ R_world2model @ np.block([np.eye(3), -cam_pos.reshape(-1,1)])
+    return cam_ex
+
+def get_cam_mat_mrp(cam_param, cam_pos, cam_mrp):
+    cam_in = get_cam_in(cam_param)
+    cam_ex = get_cam_ex_mrp(cam_pos, cam_mrp)
+    return inv_SE2 @ cam_in @ cam_ex
+
+def get_cam_ex_dcm(cam_pos, R):
+   cam_ex = R_model2cam @ R @ np.block([np.eye(3), -cam_pos.reshape(-1,1)])
+   return cam_ex
+
+def get_cam_mat_dcm(cam_param, cam_pos, R):
+    cam_in = get_cam_in(cam_param)
+    cam_ex = get_cam_ex_dcm(cam_pos, R)
+    return inv_SE2 @ cam_in @ cam_ex
+
+def get_ray(K, R, p_h):
+    # x_h = K R [I | -C] X_h
+    # x_h = KR(X - C)
+    # inv(KR) x_h = X-C = v
+    R = R_model2cam @ R
+    Bp = np.linalg.inv(K @ R)
+    v = Bp @ p_h
+    v = v / np.linalg.norm(v)
+    return v
 ## Resampling based on the examples at: https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/12-Particle-Filters.ipynb
 ## originally by Roger Labbe, under an MIT License
 def systematic_resample(weights):